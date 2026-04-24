79b2266bb3acc5607e0a64b46fd5b11f726dadf1
Yair Lenga <yair.lenga@gmail.com>
2026-04-24 16:22:04 +0300

    Move articles to sub folder

--- test2.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh -uex
-CFLAGS="-Iapi/include"
-gcc $CFLAGS -c -g test2.c 
-./build/dwarf-query/enum_dwarf_query --format=c enum_2.o > enum_test2.c
-gcc $CFLAGS -c -g enum_test2.c -Iapi/include
-gcc $CFLAGS -g -o test2.exe test2.c enum_test2.c api/src/enum_desc.c -Iapi/include
-./test2.exe
